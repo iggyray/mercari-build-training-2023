774a52a03937faa2eeee7cb46cce6c21077b8db7
iggy <iggynelo@gmail.com>
2023-05-19 14:29:31 +0900

    step 7 task 4: Changed css styling, and used grid layout for itemlist

--- typescript/simple-mercari-web/src/App.tsx
+++ typescript/simple-mercari-web/src/App.tsx
@@ -1,14 +1,22 @@
-import { useState } from 'react';
-import './App.css';
-import { ItemList } from './components/ItemList';
-import { Listing } from './components/Listing';
+import { useState } from "react"
+import "./App.css"
+import { ItemList } from "./components/ItemList"
+import { Listing } from "./components/Listing"
 
 function App() {
   // reload ItemList after Listing complete
-  const [reload, setReload] = useState(true);
+  const [reload, setReload] = useState(true)
   return (
-    <div>
-      <header className='Title'>
+    <div className="main">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <header className="Title">
         <p>
           <b>Simple Mercari</b>
         </p>
@@ -23,4 +31,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
--- typescript/simple-mercari-web/src/components/Listing/Listing.tsx
+++ typescript/simple-mercari-web/src/components/Listing/Listing.tsx
@@ -1,66 +1,92 @@
-import React, { useState } from 'react';
+import React, { useState } from "react"
 
-const server = process.env.REACT_APP_API_URL || 'http://127.0.0.1:9000';
+const server = process.env.REACT_APP_API_URL || "http://127.0.0.1:9000"
 
 interface Prop {
-  onListingCompleted?: () => void;
+  onListingCompleted?: () => void
 }
 
 type formDataType = {
-  name: string,
-  category: string,
-  image: string | File,
+  name: string
+  category: string
+  image: string | File
 }
 
 export const Listing: React.FC<Prop> = (props) => {
-  const { onListingCompleted } = props;
+  const { onListingCompleted } = props
   const initialState = {
     name: "",
     category: "",
     image: "",
-  };
-  const [values, setValues] = useState<formDataType>(initialState);
+  }
+  const [values, setValues] = useState<formDataType>(initialState)
 
   const onValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValues({
-      ...values, [event.target.name]: event.target.value,
+      ...values,
+      [event.target.name]: event.target.value,
     })
-  };
+  }
   const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValues({
-      ...values, [event.target.name]: event.target.files![0],
+      ...values,
+      [event.target.name]: event.target.files![0],
     })
-  };
+  }
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const data = new FormData()
-    data.append('name', values.name)
-    data.append('category', values.category)
-    data.append('image', values.image)
+    data.append("name", values.name)
+    data.append("category", values.category)
+    data.append("image", values.image)
 
-    fetch(server.concat('/items'), {
-      method: 'POST',
-      mode: 'cors',
+    fetch(server.concat("/items"), {
+      method: "POST",
+      mode: "cors",
       body: data,
     })
-      .then(response => {
-        console.log('POST status:', response.statusText);
-        onListingCompleted && onListingCompleted();
+      .then((response) => {
+        console.log("POST status:", response.statusText)
+        onListingCompleted && onListingCompleted()
       })
       .catch((error) => {
-        console.error('POST error:', error);
+        console.error("POST error:", error)
       })
-  };
+  }
   return (
-    <div className='Listing'>
+    <div className="Listing container-fluid">
       <form encType="multipart/form-data" onSubmit={onSubmit}>
-        <div>
-          <input type='text' name='name' id='name' placeholder='name' onChange={onValueChange} required />
-          <input type='text' name='category' id='category' placeholder='category' onChange={onValueChange} />
-          <input type='file' name='image' id='image' onChange={onFileChange} required />
-          <button type='submit'>List this item</button>
+        <div className="input-group">
+          <input
+            className="form-control my-auto"
+            type="text"
+            name="name"
+            id="name"
+            placeholder="name"
+            onChange={onValueChange}
+            required
+          />
+          <input
+            className="form-control my-auto"
+            type="text"
+            name="category"
+            id="category"
+            placeholder="category"
+            onChange={onValueChange}
+          />
+          <input
+            className="form-control form-control-sm my-auto"
+            type="file"
+            name="image"
+            id="image"
+            onChange={onFileChange}
+            required
+          />
+          <button type="submit" className="btn btn-outline-danger my-auto">
+            List this item
+          </button>
         </div>
       </form>
     </div>
-  );
+  )
 }
